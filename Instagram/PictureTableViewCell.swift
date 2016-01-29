fd5ce90982f71e3c3b6392c3e93c521f2f7c5cff
Jeanne Nina <owner@MacBook-Pro.local>
2016-01-28 20:41:25 -0500

    initial commit

---
+++ Instagram/PictureTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PictureTableViewCell.swift
+//  Instagram
+//
+//  Created by Jeanne Nina on 1/28/16.
+//  Copyright © 2016 Nina. All rights reserved.
+//
+
+import UIKit
+
+class PictureTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
